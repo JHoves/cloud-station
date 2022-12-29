233618632867fdbb0c62fcba7360f76627548b4e
jhoves <2332689115@qq.com>
2022-12-29 19:33:18 +0800

    验证文件上传的oss

--- example/oss_test.go
+++ example/oss_test.go
@@ -33,18 +33,23 @@ func TestBucketList(t *testing.T) {
 
 // 测试上传文件的功能是否正常
 func TestUploadFile(t *testing.T) {
-	bucket, err := client.Bucket("my-bucket")
+	bucket, err := client.Bucket(BucketName)
 	if err != nil {
 		t.Log(err)
 	}
 
-	err = bucket.PutObjectFromFile("my-object", "LocalFile")
+	//上传文件到bucket中
+	//常见我们文件，需要创建一个文件夹（但是云商ossServer会根据你的key路径结果，自动帮你创建目录）
+	//objectKey：上传到bucket里面的对象的名称（包含路径）
+	//mydir/test.go，oss server会自动创建一个mydir目录，相当于mkdir -pv
+	//把当前这个文件上传到了mydir下
+	err = bucket.PutObjectFromFile("mydir/test.go", "oss_test.go")
 	if err != nil {
 		t.Log(err)
 	}
 }
 
-// 初始话一个Oss Client 等下其他测试用例可以直接使用
+// 初始化一个Oss Client 等下其他测试用例可以直接使用
 func init() {
 	c, err := oss.New(OssEndpoint, AccessKey, AccessSecret)
 	if err != nil {
